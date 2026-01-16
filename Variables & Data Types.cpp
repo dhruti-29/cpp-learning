216c889d5fddbb9d448ccf287949299a8a418b99
dhruti viradiya <25cs113@charusat.edu.in>
2026-01-16 10:20:49 +0530

    add new c++ file

---
+++ Variables & Data Types.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+int num1,num2;
+
+//first number input
+cout<<"enter number \n";
+cin>>num1;
+cout<<"the number is "<<num1<<endl;
+//second number input
+cout<<"enter second number : \n";
+cin>>num2;
+cout<<"the second number is :"<<num2<<endl;
+
+//for adition and diffrence 
+cout<<"addition value is : "<<num1+num2<<endl;
+cout<<"diffrence is :"<<num1 - num2<<endl;
+
+
+//for area caclulation 
+int r;
+cout<<"enter radius :"<<endl;
+cin>>r;
+cout<<"area :"<<3.14 * r * r<<endl;
+}
