9dd038d09e0d8d0804a547dd7ca94d453b00d9d6
dhruti viradiya <25cs113@charusat.edu.in>
2026-01-16 10:19:36 +0530

    add new c++ file

---
+++ Type Casting.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+float a=45.56;
+    cout<<"value is a "<<a<<endl;
+    cout<<"value is a "<<(int)a;
+
+    // ⁠Write a program to input an integer and 
+    // display its ASCII character. (Use char/int type
+    // casting)
+
+    char ch;
+    cout<<"\n enter a character :";
+    cin>>ch;
+    cout<<"ascii value of : "<<int(ch);
+
+
+}
