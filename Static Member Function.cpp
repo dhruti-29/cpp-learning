6d27e0646a4b8c020e34e0d5bb4471ac04a798d9
dhruti viradiya <25cs113@charusat.edu.in>
2026-02-02 23:53:21 +0530

    add new cpp file

--- Static Member Function.cpp
+++ Static Member Function.cpp
@@ -1,6 +1,28 @@
 #include <iostream>
+using namespace std;
+class employee{
+    public:
+static int count ;
+int id = 0;
+employee(){
+    count++;
+}
 
+static void countt(){
+
+    cout<<"ther id no is : "<<count;
+
+
+}
+
+};
+
+int employee :: count =0;
 int main() {
+
+employee e1,e2;
+employee::countt();
+
     
     return 0;
 }
