4c0b63e84bf39e39c18fb63fb8c97e467cf47001
dhruti viradiya <25cs113@charusat.edu.in>
2026-02-16 19:09:25 +0530

    add new

---
+++  This Pointer.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+class student{
+int id;
+string name;
+
+public:
+student(int id ,string name){
+    this->id = id;
+    this->name = name;
+}
+    
+void display(){
+    cout<<"NAME : "<<name<<endl;
+    cout<<"ID : "<<id<<endl;
+
+}
+};
+int main() {
+    student s1(113,"dhruti");
+    s1.display();
+    return 0;
+}
