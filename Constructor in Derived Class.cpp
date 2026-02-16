70f479097bc3df21781296100c23efd2fabb7900
dhruti viradiya <25cs113@charusat.edu.in>
2026-02-16 19:10:33 +0530

    add new cpp file

---
+++ Constructor in Derived Class.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+using namespace std;
+
+class person{
+    public :
+string name;
+int age;
+
+
+    person(string n,int a){
+name =n ;
+age = a;
+
+    }
+
+};
+
+
+
+class employee : public person{
+
+    int id ;
+
+    public :
+//this main difference
+// inshort inhertance ni jem constructor ....
+    employee(string n, int a, int i) : person(n,a){
+id =i;
+// Initialization list (Base constructor call)
+    }
+
+    void display(){
+
+        cout<<"****DETAILS****"<<endl;
+        cout<<"NAME : "<<name<<endl;
+        cout<<"ID : "<<id<<endl;
+        cout<<"AGE : "<<age<<endl;
+
+    }
+
+};
+int main() {
+    
+    employee e1("dhruti" , 17 , 113);
+
+    e1.display();
+    return 0;
+}
