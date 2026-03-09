671f18a44510ab3eef90c4855ade800b59ceccad
dhruti viradiya <25cs113@charusat.edu.in>
2026-03-09 23:21:02 +0530

    add new cpp file

---
+++ Constructors.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+
+class car{
+
+string name;
+int price;
+
+public :
+
+car(){
+
+    cout<<"enter details"<<endl;
+
+    cout<<"enter name :"<<endl;
+    cin>>name;
+
+    cout<<"enter price :"<<endl;
+    cin>>price;
+
+}
+
+
+};
+int main() {
+    
+    return 0;
+}
