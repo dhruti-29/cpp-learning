b64e3828b8c6d59af8b139af2e87a55bdf6f0911
dhruti viradiya <25cs113@charusat.edu.in>
2026-03-18 23:04:38 +0530

    add cpp file

---
+++ Hospital Record System.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+
+class hospital{
+
+string name;
+int age;
+public:
+
+
+};
+
+class doctors :public hospital{
+
+
+
+
+};
+
+class patients: public patients{
+
+
+    
+};
+int main() {
+    
+    return 0;
+}
