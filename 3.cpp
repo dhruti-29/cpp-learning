99cbb8e13fc3d09ad37339068bbd2d09eee07adf
dhruti viradiya <25cs113@charusat.edu.in>
2026-03-22 22:11:35 +0530

    add cpp file

---
+++ 3.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+class p{
+public:
+
+void patteren3(int n){
+    for(int i=0;i<=n;i++){
+        for(int j=0;j<=i;j++){
+            cout<<i;
+        }
+        cout<<endl;
+    }
+}
+};
+
+int main() {
+    p p2;
+    p2.patteren3(5);
+    return 0;
+}
