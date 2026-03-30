a74e585522cdb06080411b323e4788733c61e371
dhruti viradiya <25cs113@charusat.edu.in>
2026-03-30 21:52:26 +0530

    add cpp file

--- Inheritance Ambiguity.cpp
+++ Inheritance Ambiguity.cpp
@@ -1,11 +1,32 @@
 #include <iostream>
 using namespace std;
 
-class student{
+class A{
+    public:
+    void show(){
+
+        cout<<"class A function"<<endl;
+
+    }
 
 
 };
+class B{
+public:
+void show(){
+    cout<<"class B function"<<endl;
+
+}
+};
+
+class C : public A , public B{
+
+};
 int main() {
-    
+    C obj;
+    //obj.show();
+    //give error
+    obj.A::show();
+    obj.B::show();
     return 0;
 }
