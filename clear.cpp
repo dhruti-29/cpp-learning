43063b1ed6d3dead71bac11dc1b9c20ce1c3115b
dhruti viradiya <25cs113@charusat.edu.in>
2026-04-21 21:05:10 +0530

    add new cpp file

---
+++ clear.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include<string>
+using namespace std;
+
+int main() {
+   string s;
+    getline(cin,s);
+
+    cout<<s<<endl;
+
+    s.clear();
+    cout<<s<<endl;
+
+    return 0;
+}
