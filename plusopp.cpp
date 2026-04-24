52d72b1ef2287d62ffeaa9cfef8da07d0372d197
dhruti viradiya <25cs113@charusat.edu.in>
2026-04-24 22:37:33 +0530

    add new cpp file

---
+++ plusopp.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    
+    string s ="hello";
+    string s1 ="dhruti";
+    string r = s +" " +s1;
+
+    cout<<"result : "<<r<<endl;
+    return 0;
+}
