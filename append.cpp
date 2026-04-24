657883b370ef28265fa5476139987ab2aa361bbe
dhruti viradiya <25cs113@charusat.edu.in>
2026-04-24 22:32:26 +0530

    add new cpp file

---
+++ append.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include<string>
+using namespace std;
+
+int main() {
+    string s1;
+
+    string s ="dhruti";
+    s1.append(s);
+    string p =".  viradiya";
+
+s1.append(p,2 , 4);
+
+cout<<s1<<endl;
+    return 0;
+}
