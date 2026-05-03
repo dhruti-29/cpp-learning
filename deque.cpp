17e0e982bc24e7552171c8618edb90b246070e2b
dhruti viradiya <25cs113@charusat.edu.in>
2026-05-03 21:56:14 +0530

    add new cpp file

---
+++ deque.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include<deque>
+using namespace std;
+
+int main() {
+
+    deque<int>d;
+    deque<int>d1(5);
+    deque<int>d2(2,233); // 2 space banne par 233 233 print thase 
+    deque<int>d4 ={1,2,3,4};
+    
+    return 0;
+}
