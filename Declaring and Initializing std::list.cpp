adf7afc5eaaeee91e7faffb473943e55f35a263d
dhruti viradiya <25cs113@charusat.edu.in>
2026-05-03 21:55:10 +0530

    add new cpp file

---
+++ Declaring and Initializing std::list.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include<list>
+using namespace std;
+
+int main() {
+    list<int>ls1;
+    list<int>ls2(2);
+    list<int>ls3(5,100);
+    list<int>ls4={2,4,5,6,7};
+    
+    return 0;
+}
