8e908f23bc3a6e4451ca6ca5503f284bc31fbb94
dhruti viradiya <25cs113@charusat.edu.in>
2026-05-04 13:16:00 +0530

    add new cpp file

---
+++ vector_basic.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include<vector>
+using namespace std;
+
+int main() {
+    
+    vector<int> vec1(5);
+    vector<int>vec2(5,234);
+    vector<int> vec3;
+    vector<int> vec4= {1,2,4};
+        return 0;
+}
