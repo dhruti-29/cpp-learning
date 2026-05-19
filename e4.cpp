60e6de07cf3cb3a5e67ff605da88ccab07b36ab3
dhruti viradiya <25cs113@charusat.edu.in>
2026-05-19 21:43:43 +0530

    add new cpp code file

--- e4.cpp
+++ e4.cpp
@@ -1,13 +1,37 @@
 #include <iostream>
 using namespace std;
 
-class system{
+class man{
+    private:
+    string name ;
+    int id;
 public:
- T calclation<T a , T b>
 
-    
+ man(){
+    cout<<"enter your details : "<<endl;
+    cout<<"enter name :"<<endl;
+    cin>>name;
+
+    cout<<"enter id :"<<endl;
+    cin>>id;
+
+ 
+}
+
+template<class T>
+ T calculation(T salary){
+    return salary * 0.100;
+ }
+ ~man(){
+    cout<<"this part is for memory cleanup"<<endl;
+}
 };
+
 int main() {
-    
+
+   man s1;
+cout << "Bonus: " << s1.calculation(2000) << endl;
+
+cout << "Bonus: " << s1.calculation(2500.50f) << endl;
     return 0;
 }
